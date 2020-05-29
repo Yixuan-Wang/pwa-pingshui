4613c39e893664b76f00c196967947d767191cfd
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2020-05-29 15:27:32 +0000

    deploy: 836f1b34c48f53e2b5f48ea2544ff58db04e5ec2

---
+++ precache-manifest.76c591fda1501c1fbbefe25be7eb7c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6434717c43921d97d39",
+    "url": "/css/app.7ed5d82a.css"
+  },
+  {
+    "revision": "49695a9ee9149776c964052da3599bcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6434717c43921d97d39",
+    "url": "/js/app.fd659e9c.js"
+  },
+  {
+    "revision": "3b2ce2878ee298bc5b42",
+    "url": "/js/chunk-vendors.28407b70.js"
+  },
+  {
+    "revision": "5f0cdb66af99bda8d1337b9ead5ff454",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
