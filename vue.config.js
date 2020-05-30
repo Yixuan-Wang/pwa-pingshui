5e52b2abac156b2f8a015fec3644c0b58eb10d61
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2020-05-30 15:21:51 +0800

    fix: config(workbox, style)

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,38 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/pwa-pingshui" : "/",
   pwa: {
     name: 'Pingshui',
-    themeColor: '#35495e',
-    msTileColor: '#35495e',
+    themeColor: '#2c3e50',
+    msTileColor: '#2c3e50',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
 
     manifestOptions: {
       start_url: '.',
-      background_color: '#4c89fe'
+      background_color: '#2c3e50'
     },
     iconPaths: {
       favicon16: 'favicon.ico',
+      favicon32: 'favicon.ico',
       appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
       msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          urlPattern: /.*/,
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'pingshui',
+            expiration: {
+              maxAgeSeconds: 86400 * 15
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
     }
   }
 }
